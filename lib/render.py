0c859aabecb17cff281822569176fb188930a6a4
hyeonjang <ippguu@gmail.com>
2023-11-01 09:35:39 +0900

    clean

--- lib/render.py
+++ lib/render.py
@@ -1,7 +1,6 @@
 import nvdiffrast.torch as dr
 import torch
 import torch.nn.functional as tfunc
-from torchvision.transforms import GaussianBlur as gaussian
 
 def _warmup(glctx):
     #windows workaround for https://github.com/NVlabs/nvdiffrast/issues/59
@@ -22,11 +21,10 @@ class Rasterizer:
             image_size: tuple[int,int],
             ):
         self._mvp = proj @ mv #C,4,4
+        self._campos = torch.linalg.inv(mv)[:, :3, 3]
         self._image_size = image_size
         self._glctx = dr.RasterizeGLContext()
         _warmup(self._glctx)
-
-        self._filter = gaussian([3, 3], sigma=0.5)
 
     def tangent(self, v_pos, v_idx, v_nrm, n_idx, v_tex, t_idx):
         vertices = v_pos[v_idx.type(dtype=torch.long)] #F,C=3,3
@@ -62,7 +60,6 @@ class Rasterizer:
         vert_hom = torch.cat((v_pos, torch.ones(V,1,device=v_pos.device)),axis=-1) #V,3 -> V,4
         vertices_clip = vert_hom @ self._mvp.transpose(-2,-1) #C,V,4
         rast_out,_ = dr.rasterize(self._glctx, vertices_clip, faces, resolution=self._image_size, grad_db=False) #C,H,W,4
-        # vert_col = (v_nrm + 1)/2 #V,3
         vert_col = v_nrm
         color,_ = dr.interpolate(vert_col, rast_out, n_idx) #C,H,W,3
         alpha = torch.clamp(rast_out[..., -1:], max=1) #C,H,W,1
@@ -88,10 +85,8 @@ class Rasterizer:
         rast_out, rast_out_db = dr.rasterize(self._glctx, vertices_clip, v_idx, resolution=resolution, grad_db=False) #C,H,W,4
          
         texc, texd = dr.interpolate(v_tex[None, ...], rast_out, t_idx, rast_db=rast_out_db, diff_attrs='all')
-        # albedo = dr.texture(texture["albedo"][0], texc, texd, mip=texture["albedo"][1:], filter_mode='linear-mipmap-linear')
         if isinstance(texture["albedo"], list):
             albedo = dr.texture(texture["albedo"][0], texc, filter_mode='linear')
-            # albedo = dr.texture(texture["albedo"][0][None, ...], texc, texd, filter_mode='linear-mipmap-linear', max_mip_level=6)
         else:
             albedo = dr.texture(texture["albedo"][None, ...], texc, texd, filter_mode='linear-mipmap-linear', max_mip_level=6)
         color = albedo
@@ -107,6 +102,7 @@ class Rasterizer:
             v_tex: torch.Tensor,
             t_idx: torch.Tensor, 
             texture: torch.Tensor, 
+            tangent_space = False,
             ) -> torch.Tensor: #C,H,W,4
 
         V = v_pos.shape[0]
@@ -115,24 +111,30 @@ class Rasterizer:
         vertices_clip = vert_hom @ self._mvp.transpose(-2,-1) #C,V,4
         rast_out, rast_out_db = dr.rasterize(self._glctx, vertices_clip, v_idx, resolution=self._image_size, grad_db=False) #C,H,W,4
     
-        # TBN
-        v_nrm = (v_nrm + 1)/2 #V,3
-        T = self.tangent(v_pos, v_idx, v_nrm, n_idx, v_tex, t_idx)
-        B = tfunc.normalize(torch.cross(T, v_nrm), dim=-1)
-        
+        # View pos
+        rast_pos,_ = dr.interpolate(v_pos.contiguous(), rast_out, v_idx.int())
+        # view_vec = tfunc.normalize(self._campos[:, None, None, :] - rast_pos)
+
         texc, texd = dr.interpolate(v_tex[None, ...], rast_out, t_idx, rast_db=rast_out_db, diff_attrs='all')
         if isinstance(texture["normal"], list):
             normal = dr.texture(texture["normal"][0], texc, filter_mode='linear')
-            # albedo = dr.texture(texture["albedo"][0][None, ...], texc, texd, filter_mode='linear-mipmap-linear', max_mip_level=6)
         else:
             normal = dr.texture(texture["normal"][None, ...], texc, texd, filter_mode='linear-mipmap-linear', max_mip_level=6)
 
-        T, tb = dr.interpolate(T, rast_out, v_idx, rast_db=rast_out_db, diff_attrs='all') #C,H,W,3
-        B, Bb = dr.interpolate(B, rast_out, v_idx, rast_db=rast_out_db, diff_attrs='all') #C,H,W,3
-        N, Nb = dr.interpolate(v_nrm, rast_out, v_idx, rast_db=rast_out_db, diff_attrs='all') #C,H,W,3
-    
-        final_normal = T * normal[..., 0:1] - B * normal[..., 1:2] + N * torch.clamp(normal[..., 2:3], min=0.0)
-        final_normal = tfunc.normalize(final_normal, dim=-1)
+        # TBN
+        v_nrm = (v_nrm + 1)/2 #V,3
+
+        if tangent_space:
+            T = self.tangent(v_pos, v_idx, v_nrm, n_idx, v_tex, t_idx)
+            B = tfunc.normalize(torch.cross(T, v_nrm), dim=-1)
+            T, tb = dr.interpolate(T, rast_out, v_idx, rast_db=rast_out_db, diff_attrs='all') #C,H,W,3
+            B, Bb = dr.interpolate(B, rast_out, v_idx, rast_db=rast_out_db, diff_attrs='all') #C,H,W,3
+            N, Nb = dr.interpolate(v_nrm, rast_out, v_idx, rast_db=rast_out_db, diff_attrs='all') #C,H,W,3
+            final_normal = T * normal[..., 0:1] - B * normal[..., 1:2] + N * torch.clamp(normal[..., 2:3], min=0.0)
+            final_normal = tfunc.normalize(final_normal, dim=-1)
+        else:
+            final_normal = normal
+
         color = final_normal
         alpha = torch.clamp(rast_out[..., -1:], max=1) #C,H,W,1
         color = torch.concat((color,alpha),dim=-1) #C,H,W,4
